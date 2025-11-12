670e1256b5c87b89bad5c3a4407992711e8e3cac
unknown <shahd34@gmail.com>
2025-11-12 22:25:06 +0200

    Testing web games on mobile

--- games/games/cross_cat/cross_cat.service.worker.js
+++ games/games/cross_cat/cross_cat.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1762876609|395217987';
+const CACHE_VERSION = '1762978777|39822137';
 /** @type {string} */
 const CACHE_PREFIX = 'Crosscat-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
--- games/games/unique_aquarium/unique_aquarium.service.worker.js
+++ games/games/unique_aquarium/unique_aquarium.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1762948240|4514659162';
+const CACHE_VERSION = '1762979078|288150287';
 /** @type {string} */
 const CACHE_PREFIX = 'Unique Aquarium-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
