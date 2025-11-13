e2ecbb053e15d113b6c7aeef8f25af8474366fa9
unknown <shahd34@gmail.com>
2025-11-13 12:39:45 +0200

    Unique Aquarium web version save download

--- games/games/unique_aquarium/unique_aquarium.service.worker.js
+++ games/games/unique_aquarium/unique_aquarium.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763021966|3598579823';
+const CACHE_VERSION = '1763030300|5853238209';
 /** @type {string} */
 const CACHE_PREFIX = 'Unique Aquarium-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
