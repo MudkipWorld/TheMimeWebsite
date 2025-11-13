1c659d5953fad701b4d4d724be9f3bbe6bc66c76
unknown <shahd34@gmail.com>
2025-11-13 14:26:17 +0200

    Unique Aquarium Fixed egg timer in different rooms

--- games/games/unique_aquarium/unique_aquarium.service.worker.js
+++ games/games/unique_aquarium/unique_aquarium.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763030300|5853238209';
+const CACHE_VERSION = '1763036688|83682696';
 /** @type {string} */
 const CACHE_PREFIX = 'Unique Aquarium-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
