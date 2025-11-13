3d2dee88a5e80bb47df6493a12f08cc4020ceb45
unknown <shahd34@gmail.com>
2025-11-13 10:20:05 +0200

    updated Unique Aquarium save and load issue

--- games/games/unique_aquarium/unique_aquarium.service.worker.js
+++ games/games/unique_aquarium/unique_aquarium.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1762998075|1419567323';
+const CACHE_VERSION = '1763021966|3598579823';
 /** @type {string} */
 const CACHE_PREFIX = 'Unique Aquarium-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
