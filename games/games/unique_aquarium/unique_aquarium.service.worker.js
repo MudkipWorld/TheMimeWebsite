94200dc4e5efb26b10c073de6a8f82fba3e075eb
unknown <shahd34@gmail.com>
2025-11-13 03:14:58 +0200

    Fixing unique aquarium bug

--- games/games/unique_aquarium/unique_aquarium.service.worker.js
+++ games/games/unique_aquarium/unique_aquarium.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1762979078|288150287';
+const CACHE_VERSION = '1762996362|1635159239';
 /** @type {string} */
 const CACHE_PREFIX = 'Unique Aquarium-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
