49cff50e09e00ef418cfb6a1afccfed37fade195
unknown <shahd34@gmail.com>
2025-11-14 13:09:34 +0200

    Audio issue on mobile web fix

--- games/games/unique_aquarium/unique_aquarium.service.worker.js
+++ games/games/unique_aquarium/unique_aquarium.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763117301|1127924277';
+const CACHE_VERSION = '1763118548|12215822';
 /** @type {string} */
 const CACHE_PREFIX = 'Unique Aquarium-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
