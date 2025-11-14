a2791b231a415ac6e4192e061723e8ed2c7d2a87
unknown <shahd34@gmail.com>
2025-11-14 13:14:57 +0200

    Why won't you get fixed on mobile

--- games/games/unique_aquarium/unique_aquarium.service.worker.js
+++ games/games/unique_aquarium/unique_aquarium.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763118724|12394013';
+const CACHE_VERSION = '1763118875|163137021';
 /** @type {string} */
 const CACHE_PREFIX = 'Unique Aquarium-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
