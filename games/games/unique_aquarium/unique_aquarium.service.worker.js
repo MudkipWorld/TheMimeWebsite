793a3181a85861d01ef322930ad5b095d79898f5
unknown <shahd34@gmail.com>
2025-11-14 12:50:00 +0200

    Unique Aquarium small web fixed

--- games/games/unique_aquarium/unique_aquarium.service.worker.js
+++ games/games/unique_aquarium/unique_aquarium.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763049091|938709553';
+const CACHE_VERSION = '1763117301|1127924277';
 /** @type {string} */
 const CACHE_PREFIX = 'Unique Aquarium-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
