38a64184cf0395a4cab495f1ccddf8f58aaf6984
unknown <shahd34@gmail.com>
2025-11-14 20:52:23 +0200

    tiny zips update

--- games/games/unique_aquarium/unique_aquarium.service.worker.js
+++ games/games/unique_aquarium/unique_aquarium.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763139172|367277196';
+const CACHE_VERSION = '1763144538|415365584';
 /** @type {string} */
 const CACHE_PREFIX = 'Unique Aquarium-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
