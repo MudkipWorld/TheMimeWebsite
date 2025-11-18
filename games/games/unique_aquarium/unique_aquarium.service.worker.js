a54b25fb39e7cbd56a55fcfed32295237ef4d936
unknown <shahd34@gmail.com>
2025-11-19 00:41:45 +0200

    Upadting web to latest

--- games/games/unique_aquarium/unique_aquarium.service.worker.js
+++ games/games/unique_aquarium/unique_aquarium.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763500962|4578041662';
+const CACHE_VERSION = '1763505601|1502023623';
 /** @type {string} */
 const CACHE_PREFIX = 'Unique Aquarium-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
