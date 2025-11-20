c77c5df74c646a2bfe7aebb5c3d9c1498c2b30b2
unknown <shahd34@gmail.com>
2025-11-20 02:09:05 +0200

    UA Bug fixes

--- games/games/unique_aquarium/unique_aquarium.service.worker.js
+++ games/games/unique_aquarium/unique_aquarium.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763559797|7371083184';
+const CACHE_VERSION = '1763597219|1641059451';
 /** @type {string} */
 const CACHE_PREFIX = 'Unique Aquarium-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
