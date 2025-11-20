082acddbefd34da7879441d1548addcdfbdd150e
unknown <shahd34@gmail.com>
2025-11-20 02:53:09 +0200

    UA Fixing breeding fish getting stuck in limbo when skipping

--- games/games/unique_aquarium/unique_aquarium.service.worker.js
+++ games/games/unique_aquarium/unique_aquarium.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763597219|1641059451';
+const CACHE_VERSION = '1763599721|261286024';
 /** @type {string} */
 const CACHE_PREFIX = 'Unique Aquarium-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
