747422de71562c53e852eb05bfa0e7c77a53ac8d
unknown <shahd34@gmail.com>
2025-11-22 01:51:22 +0200

    a Tiny UA Update

--- games/games/unique_aquarium/unique_aquarium.service.worker.js
+++ games/games/unique_aquarium/unique_aquarium.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763666997|51446256';
+const CACHE_VERSION = '1763769053|35463619';
 /** @type {string} */
 const CACHE_PREFIX = 'Unique Aquarium-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
