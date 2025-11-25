d29f91999dc85065bc44fd8a5872504aad6c3505
unknown <shahd34@gmail.com>
2025-11-25 20:37:54 +0200

    fixing axolotl shared info bug

--- games/games/unique_aquarium/unique_aquarium.service.worker.js
+++ games/games/unique_aquarium/unique_aquarium.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1764089781|187452782';
+const CACHE_VERSION = '1764095782|60406538';
 /** @type {string} */
 const CACHE_PREFIX = 'Unique Aquarium-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
